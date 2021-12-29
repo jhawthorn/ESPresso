8931c3878de3c5ce4c2fb8fad363488a2c8b72db
John Hawthorn <john@hawthorn.email>
2021-12-28 18:11:51 -0800

    Use vTaskDelayUntil

--- src/main.cc
+++ src/main.cc
@@ -81,8 +81,13 @@ void heating_state_set(bool on) {
 
 void temperature_loop()
 {
-    ESP_LOGI("Temperature", "Starting loop");
     vTaskDelay(pdMS_TO_TICKS(1000));
+
+    ESP_LOGI("Temperature", "Starting loop");
+
+    TickType_t lastWakeTime;
+    const TickType_t frequency = pdMS_TO_TICKS(1000);
+    lastWakeTime = xTaskGetTickCount();
 
     while (true) {
         uint16_t rtd;
@@ -102,7 +107,7 @@ void temperature_loop()
             heating_state_set(temp < target_temperature);
         }
 
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        vTaskDelayUntil(&lastWakeTime, frequency);
     }
 }
 
